25d3d229ced857df121a61f3d9a960ebb10d0095
German Rodriguez <gggermanrz@gmail.com>
2026-03-27 00:12:01 -0300

    punto 3 realizado

--- 2do-anio/1er cuatrimestre/AyED/src/tp1/ejercicio3/Test.java
+++ 2do-anio/1er cuatrimestre/AyED/src/tp1/ejercicio3/Test.java
@@ -1,21 +1,33 @@
 package tp1.ejercicio3;
-import java.util.ArrayList;
 	public class Test {
 		public static void main (String [] args) {
-			Estudiante est = new Estudiante();
-			Profesor pf = new Profesor();
 			
 			Estudiante [] arregloE = new Estudiante [2];
 			Profesor [] arregloP = new Profesor [3];
 			
+			arregloE[0] = new Estudiante();
+			arregloE[1] = new Estudiante();
+			
+			arregloP[0] = new Profesor();
+			arregloP[1] = new Profesor();
+			arregloP[2] = new Profesor();
 			
 			for(int i = 0; i < arregloE.length;i++) {
-				arregloE[i].setNombre("carlos");
-				arregloE[i].setApellido("tevez");
+				arregloE[i].setNombre("carlos+i");
+				arregloE[i].setApellido("tevez+i");
 				arregloE[i].setComision(4);
-				arregloE[i].setDire("n961");
-				arregloE[i].setEmail("jj");
+				arregloE[i].setDire("n961+i");
+				arregloE[i].setEmail("jj+i");
 				System.out.println(arregloE[i].tusDatos());
+			}
+			for (int i = 0; i < arregloP.length;i++) {
+				arregloP[i].setNombre("juan+i");
+				arregloP[i].setApellido("Perez+i");
+				arregloP[i].setCatedra("fm+i");
+				arregloP[i].setComision(3);
+				arregloP[i].setEmail("gmail+i");
+				arregloP[i].setFacultad("UNLP+i");
+				System.out.println(arregloP[i].tusDatos());
 			}
 			
 		}
