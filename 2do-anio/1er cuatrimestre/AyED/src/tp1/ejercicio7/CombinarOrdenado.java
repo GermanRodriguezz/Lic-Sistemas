5d0df016a0b319a7fd1212ac8d60c839acae8b6a
German Rodriguez <gerdepo04mail.com>
2026-04-03 14:29:03 -0300

    Aclaraciones ayed 7j & comienzo Punto 7 obj1

--- 2do-anio/1er cuatrimestre/AyED/src/tp1/ejercicio7/CombinarOrdenado.java
+++ 2do-anio/1er cuatrimestre/AyED/src/tp1/ejercicio7/CombinarOrdenado.java
@@ -23,16 +23,16 @@ public class CombinarOrdenado {
 	}
 	
 	public ArrayList<Integer> recorrer(ArrayList<Integer> l1,ArrayList<Integer> l2,ArrayList<Integer> nuev, int pos1, int pos2){
-			if (pos1 == l1.size()) {
-				return agregarRestante(nuev, l2, pos2);
+			if (pos1 == l1.size()) { // si llega al final de la lista
+				return agregarRestante(nuev, l2, pos2); // agrega los restantes de la otra lista
 			}
-			if (pos2 == l2.size()) {
+			if (pos2 == l2.size()) { 
 				return agregarRestante(nuev, l1, pos1);
 			}
 			else {
-				if (l1.get(pos1) < l2.get(pos2)) {
-					nuev.add(l1.get(pos1));
-					 return recorrer(l1,l2,nuev,pos1+1,pos2);
+				if (l1.get(pos1) < l2.get(pos2)) { // no llego al final entonces compara
+					nuev.add(l1.get(pos1)); 		//agrega
+					 return recorrer(l1,l2,nuev,pos1+1,pos2); // llamo recursivamente y aumento una posicion en la lista donde encontre el minimo
 				}
 				else {
 					nuev.add(l2.get(pos2));
---
+++ 2do-anio/2do cuatrimestre/Oobj1/Material Redictado/Ejercicio7-FigurasCuerpos/src/main/java/ar/edu/unlp/info/oo1/Ejercicio7_FigurasCuerpos/Cuadrado.java
@@ -0,0 +1,5 @@
+package ar.edu.unlp.info.oo1.Ejercicio7_FigurasCuerpos;
+
+public class Cuadrado {
+
+}
